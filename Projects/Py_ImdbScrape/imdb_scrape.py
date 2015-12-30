71b948b24c6f1e4c1b87ffbf5452abd4fee1fb26
mohanvarma <varma.mohan@ymail.com>
2015-12-29 23:40:56 -0500

    Adding GUI support to display poster

--- Projects/Py_ImdbScrape/imdb_scrape.py
+++ Projects/Py_ImdbScrape/imdb_scrape.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 
 #Optional, for UI
+#Install these modules for best experience
 foundGtk = False
 foundWebkit = False
 
@@ -21,6 +22,13 @@ except ImportError:
 	print "Webkit module not found"
 
 BASE_URL = "http://www.imdb.com"
+
+# Print a sequence of "="
+def printBanner():
+	banner = ""
+	for i in range(60):
+		banner += "="
+	print banner
 
 class imdbScrape:
 	def __init__(self, category, string):
@@ -59,25 +67,62 @@ class imdbScrape:
 
 		firstResultUrlPath = firstResult.find("a").get("href")
 		self.url = BASE_URL+firstResultUrlPath
-		print self.url
+		printBanner()
+		print "URL: " + self.url
+		printBanner()
 	
 	def openTitleUrl(self):
 		result = requests.get(self.url)
 
 		soup = BeautifulSoup(result.content)
-		pass
+		ratingTag = soup.find("div", "ratingValue")
+		rating = ratingTag.find("strong").get("title")
+		print 'Rating: ' + rating
+		printBanner()
+
+		subTextTag = soup.find("div", "subtext")
+		aTags = subTextTag.findAll("a")
+		#print aTags
+
+		#find image from the poster tag
+		imgUrl = soup.find("div", "poster")
+		imgUrl = imgUrl.find("img").get("src")
+		self.imgUrl = imgUrl
+		
+		genreList = ""
+		releaseDate = str(aTags.pop().contents[0])
+		
+		if (self.category == 1):
+			print "TV Show Aired: " + releaseDate[:-1]
+		else:
+			print "Movie Release date: " + releaseDate[:-1]
+		
+		# Get a list of genres
+		for a in aTags:
+			a = str(a)
+			soup = BeautifulSoup(a)
+			genre = str(soup.find("span").contents)
+			genreList += str(genre)
+			genreList += ", "
+
+		
+		printBanner()
+		print "Genre: " + genreList[:-2]
+
 
 	def openImageThumbnail(self):
 		if foundGtk == False or foundWebkit == False:
 			return
 		window = gtk.Window()
-		window.set_size_request(214, 317)
+		window.set_size_request(182, 268)
 		webview = webkit.WebView()
 		window.add(webview)
 		window.show_all()
-		#webview.load_uri(self.imgUrl)
-		webview.load_uri('http://ia.media-imdb.com/images/M/MV5BMTAwNDEyODU1MjheQTJeQWpwZ15BbWU2MDc3NDQwNw@@._V1_SY317_CR0,0,214,317_AL_.jpg')
+		webview.load_uri(self.imgUrl)
+		#webview.load_uri('http://ia.media-imdb.com/images/M/\
+		#	MV5BMTAwNDEyODU1MjheQTJeQWpwZ15BbWU2MDc3NDQwNw@@._V1_SY317_CR0,0,214,317_AL_.jpg')
 		gtk.main()
+		return
 
 
 if __name__=="__main__":
@@ -88,4 +133,5 @@ if __name__=="__main__":
 
 	test = imdbScrape(category, title)
 	test.searchImdb()
+	test.openTitleUrl()
 	test.openImageThumbnail()
